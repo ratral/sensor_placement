c6b462720c503f9cd9fecc3f748f2bb3e9113ef9
Dr. Raul Trujillo Alvarez <dr.ing.trujillo@gmail.com>
2019-02-06 13:36:21 +0100

    Update network_visualization.R

--- network_visualization.R
+++ network_visualization.R
@@ -7,109 +7,95 @@ rm(list=ls())
 
 # Installs libraries 
 library(tidyverse)
+library(tidygraph)
 library(epanetReader)
 library(epanet2toolkit)
-library(visNetwork)
-library(igraph)
+library(jsonlite)
 
-# Initialize params
-params <- list(base_network    = "base_dma_01", 
-               new_network     = "base_dma_w_leaks",
-               functs_name     = "epanet_api_functions",
-               inlet_valves    = "^PRV_",
-               jt_to_analyze   = "^JT_0[A-K]", # RegExp
-               pipe_to_analyze = "PS_"         # RegExp
-               ) 
+paths <- list( data =  file.path(getwd(),"data"),
+               reports = file.path(getwd(),"reports"))
 
-# initialize files paths and files
-work_folders <- list( dir_work   = getwd(),
-                      dir_report = file.path(getwd(),"reports"), 
-                      dir_data   = file.path(getwd(),"data"),  
-                      dir_bin    = file.path(getwd(),"reports"),
-                      dir_func   = file.path(getwd(),"func"))
+params       <- "base_dma_02.json"  # File name 
 
-f_names  <- list( base_file_inp    = file.path(work_folders$dir_data, 
-                                               paste0(params$base_network,".inp")),
-                  base_file_report = file.path(work_folders$dir_report, 
-                                               paste0(params$base_network,".rpt")),
-                  new_file_inp     = file.path(work_folders$dir_data, 
-                                               paste0(params$new_network,".inp")),
-                  new_file_report  = file.path(work_folders$dir_report,
-                                               paste0(params$new_network,".rpt")),
-                  file_func        = file.path(work_folders$dir_func, 
-                                               paste0(params$functs_name,".R")))
+params       <- file.path(paths$data,params)
+params       <- fromJSON(params, flatten=TRUE)
 
-# Load Functions Standard
-source(f_names$file_func)
 
 #...............................................................................
 # 2. Read network information from an *.inp
 #...............................................................................
 
-base_input <- read.inp(f_names$base_file_inp)
+nw_input <- read.inp(file.path(paths$data,params$input_files))
 
 #...............................................................................
 # 3. Running a Full Simulation                                             ####
 #...............................................................................
 
-ENepanet(f_names$base_file_inp, f_names$base_file_report)
+ENepanet(file.path(paths$data,params$input_files), 
+         file.path(paths$data,params$output_files$report))
 
-base_report   <- read.rpt(f_names$base_file_report)
+nw_report <- read.rpt(file.path(paths$data,params$output_files$report))
 
+#...............................................................................
+# Select nodes & pipes
+#...............................................................................
 
-# Select nodes
-nodes <- eval_nodes (base_report,
-                     node_type = "",
-                     id_nodes  = "", 
-                     group = TRUE, standardize = TRUE) %>%
-         select(ID,p_median)
+nodes <- as_tibble(nw_report$nodeResults) %>%
+         group_by(ID, nodeType) %>%
+         summarise(Pressure=mean(Pressure, na.rm = TRUE))
 
+links <- nw_report$linkResults %>%
+         group_by(ID, linkType) %>%
+         summarise(Flow = mean(Flow, na.rm = TRUE)) 
 
-# Select pipes 
-pipes         <- strc_pipes(base_input, "", TRUE)
+#...............................................................................
 
-pipes_f_base  <- eval_pipes ( base_report,
-                              value = "Flow", 
-                              group = TRUE, 
-                              standardize = FALSE)
+pipes  <- as_tibble(nw_input$Pipes) %>% 
+          select(ID, Node1,Node2, Length, Diameter)
 
-pipes <- full_join( pipes, pipes_f_base, by = "ID")
+if (!is.null(nw_input$Valves)){
+  
+  temp_tab <- as_tibble(nw_input$Valves) %>%
+              select(ID, Node1,Node2, Diameter) %>%
+              mutate(Length = NA) %>%
+              select(ID, Node1, Node2, Length, Diameter)
+  
+  pipes <- bind_rows(pipes, temp_tab)
+  
+}
 
-# change from_node and to_node in function of the Flow direction 
-df <- pipes
+if (!is.null(nw_input$Pumps)){
 
-df$from_node[pipes$f_median < 0 ]  <-  pipes$to_node[pipes$f_median < 0] 
-df$to_node  [pipes$f_median < 0 ]  <-  pipes$from_node[pipes$f_median < 0] 
-df$f_median [pipes$f_median < 0 ]  <-  abs(pipes$f_median[pipes$f_median < 0])
+  temp_tab <- as_tibble(nw_input$Pumps) %>%
+              select(ID, Node1, Node2) %>%
+              mutate(Length = NA, Diameter = NA) %>%
+              select(ID, Node1, Node2, Length, Diameter)
+  pipes <- bind_rows(pipes, temp_tab)
+}
 
-# standardize df
-l <- length(df)
-maxs    <- max(df$f_median) 
-mins    <- min(df$f_median)
+links <- full_join(links, pipes, by = "ID") %>% 
+         select(Node1, Node2, Length, ID, Diameter, Flow, linkType)
 
-scaled  <- scale( df$f_median, 
-                  center = mins, 
-                  scale = maxs - mins)*100
+links <- links %>% rename(from = "Node1", to ="Node2")
 
-df$f_median <- scaled
+remove(pipes)
 
-# generate data for the visNetwork
+#-------------------------------------------------------------------------------
+# The tbl_graph object
+#-------------------------------------------------------------------------------
 
-nodes <- data.frame( id     = nodes$ID, 
-                     value  = nodes$p_median, 
-                     shadow = TRUE)
+water_network <- tbl_graph(nodes = nodes, edges = links, directed = TRUE )
 
-edges <- data.frame( from   = df$from_node, 
-                     to     = df$to_node,
-                     value  = df$f_median,
-                     arrows = "to",
-                     shadow = TRUE)
+#-------------------------------------------------------------------------------
 
-# visNetwork
-visNetwork(nodes, edges ,
-           main = "A really simple example", 
-           width = "100%") %>%
-  visLayout(randomSeed = 12)# to have always the same network
+# Attributes
 
+vertex_attr(water_network)
 
+edge_attr(ater_network) #!!!
 
+# Visualization
+
+plot( water_network, 
+      layout = layout_with_graphopt, 
+      edge.arrow.size = 0.2)
